0d35bf5d35ef753e75bc59485831dd1a9eed1951
oomuranatsumi <natsu7mikan23@gmail.com>
2022-04-23 16:56:38 +0200

    今日の作業

--- R-project.R
+++ R-project.R
@@ -1,38 +1,32 @@
-setwd("~/Desktop/Prigramming in R/Project")
+library(ggplot2)
+
+#setwd("~/Desktop/Prigramming in R/Project")
 df <- read.csv("SSDSE-B-2021.csv", header=TRUE, row.names = NULL, stringsAsFactors=FALSE, fileEncoding = "shift-jis")
-options(digits=2) #change digit
 print(df)
 
 #extraction based on columnList
 columnList <- c("SSDSE.B.2021", "Code", "Prefecture", "A1101", "A1301", "E6502") #make columnList
 data.selected <- df[, columnList] #access data
 data.selected <- data.selected[-1,]
-colnames(data.selected) <- c("year", "code of prefecture", "prefecture", "total population", "population younger than 15 yesrs old", "popularion guraduated from bachelor")
-#columnListtoNumeric <- c("total population", "population younger than 15 yesrs old", "popularion guraduated from bachelor")
-#as.numeric(data.selected[, columnListtoNumeric])
+colnames(data.selected) <- c("year", "code of prefecture", "prefecture", "total population", "population younger than 15 yesrs old", "popularion graduated from bachelor")
 print(data.selected)
 
 #make a plot of number of people who graduate from the bachelor by year
 years <- unique(data.selected$year)
-total_people_with_bachelor <- data.frame(matrix(rep(NA, 2), nrow=1))[numeric(0), ]
-colnames(total_people_with_bachelor) <- c("year", "total_people_with_bachelor")
+total_people_with_bachelor = data.frame(matrix(ncol = 1, nrow = length(years)))
+rownames(total_people_with_bachelor) <- years
 
-#is.vector(years)
 count <- 0
 for (i in years) {
-  data_year <- data.selected.total[data.selected$year == i, ]
+  data_year <- data.selected[data.selected$year == i, ]
   print(data_year)
-  total_people_with_bachelor[count + 1 , 1] <- i
-  total_people_with_bachelor[count + 1 , 2] <- sum(as.numeric(data_year[, "popularion guraduated from bachelor"]))
+  #total_people_with_bachelor[count + 1 , 1] <- i
+  total_people_with_bachelor[count + 1, 1] <- sum(as.numeric(data_year[, "popularion graduated from bachelor"]))
   count <- count + 1
   print(count)
-  }    
+}    
+barplot(total_people_with_bachelor[,1], main = "Numbers of those who graduated from the bachelor")
 
+#Summary of Statistics
 
-print(total_people_with_bachelor)
-
-barplot(total_people_with_bachelor[, 2])
-
-#summary of statistics
-
-
+data.selected
